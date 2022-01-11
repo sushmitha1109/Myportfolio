76749ce15263e26ecd90ad9cf525d3930aac6eac
OEM Configuration (temporary user) <sushmithakumaravel11@gmail.com>
2022-01-11 14:27:43 +0530

    final

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,8 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitle: `Sushmitha's portfolio`,
+    siteTitleAlt: `Sushmitha-Portfolio`,
     siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
     siteUrl: `https://cara.lekoarts.de`,
     siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
